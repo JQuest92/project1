e00c9cec0472c11b5efbf80b5691b4fa6c78e267
rsuttles58 <robert.suttles58@gmail.com>
2019-03-13 23:03:32 -0500

    Built most of weather table. With ajax calls working.

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,20 +14,47 @@ var database = firebase.database(); //database refrence
 
 //weather API AJAX call.
 function weather() {
+  // $("#weatherHolder").empty();
   //Need to change "Nashville" in the queryURL to the defined user input variable for city.  
   var weatherAPI = "8010786867558f69f90f520535222da2";
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=nashville&units=imperial&appid=" + weatherAPI;
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" +
+    "q=" + cty + "&units=imperial&appid=" + weatherAPI;
 
+    $("#weatherHolder").empty();
+    
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
     console.log(response);
+    console.log("Date:", response.list[10].dt_txt.substring(0,10));
+
+    var headers = $("<tr>").append(
+      $("<th>").text("Date"),
+      $("<th>").text("Weather"),
+      $("<th>").text("Temp"),
+    )
+
+    $("#weatherHolder > thead").append(headers);
+
+
+    for (var i = 0; i < response.length; i+8) {
+      var date = response.list[i].dt_txt.substring(0,10);
+      var weatherIcon = response.list[i].weather.icon;
+      var temp = response.list[i].main.temp;
+
+      var newDay = $("<tr>").append(
+        $("<td>").text(date),
+        $("<td>").text(weatherIcon),
+        $("<td>").text(temp)
+      );
+      $("#weatherHolder > tbody").append(newDay);
+    }
+
   });
 }
 
-$(document).ready(weather());{
+$(document).ready(); {
 
 }
 
@@ -37,7 +64,7 @@ function ticketmaster() {
 
   $("#tEvents").empty();
 
-  console.log("MADE IT HERE!");
+  // console.log("MADE IT HERE!");
   var apiKey = "AGoGvNzEmLC1H1XIJtVipXmDeRDKMPQT";
 
   var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + cty + "&apikey=" + apiKey;
@@ -45,16 +72,16 @@ function ticketmaster() {
 
   $.ajax({
     url: queryURL,
-    method: "GET"}).then(function(response) {
-        console.log(response._embedded.events);
-        for(var i = 0; i < response._embedded.events.length; i++)
-        {
-            var name = response._embedded.events[i].name;
-            var url = response._embedded.events[i].url;
+    method: "GET"
+  }).then(function (response) {
+    console.log(response._embedded.events);
+    for (var i = 0; i < response._embedded.events.length; i++) {
+      var name = response._embedded.events[i].name;
+      var url = response._embedded.events[i].url;
 
-            $("#tEvents").append('<a target="_blank" href="' + url + '">' + name + "</a><br/>");
+      $("#tEvents").append('<a target="_blank" href="' + url + '">' + name + "</a><br/>");
 
-        }
+    }
 
   });
 
@@ -62,7 +89,7 @@ function ticketmaster() {
 
 // function to get the Zomato city ID
 function cityId() {
-  var ctyId  = "";
+  var ctyId = "";
   // Ajax GET request to Zomato API URL passing the cty (city) variable
   $.ajax({
     method: "GET",
@@ -71,14 +98,14 @@ function cityId() {
     dataType: "json",
     async: true,
     headers: {
-    "user-key": "0a661374a6b58eb2fa84142d27fe81ca"
-    }, 
-    success: function(data) {
+      "user-key": "0a661374a6b58eb2fa84142d27fe81ca"
+    },
+    success: function (data) {
       ctyId = data.location_suggestions[0].id;
       // calling the Zomato function with the city Id passed in
       zomato(ctyId);
-    }, 
-    error: function() {
+    },
+    error: function () {
       console.log("error!");
     }
   })
@@ -92,25 +119,25 @@ function zomato(ctyId) {
     "url": "https://developers.zomato.com/api/v2.1/search?&city_id=" + ctyId + "&count=10",
     "method": "GET",
     "headers": {
-    "user-key": "fbf079a12e07d58c2e028ec67f02a6e1 ",
-    'Content-Type': 'application/x-www-form-urlencoded'
+      "user-key": "fbf079a12e07d58c2e028ec67f02a6e1 ",
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
   }
-  $.ajax(queryURL, function() {
-  }).then(function(response) {
+  $.ajax(queryURL, function () {
+  }).then(function (response) {
 
     var results = response;
     // loop to get 10 restaurants from the Zomato API
     for (var i = 0; i < results.restaurants.length; i++) {
       var restaurantPath = results.restaurants[i].restaurant;
-        var name = restaurantPath.name;
-        var cuisines = restaurantPath.cuisines;
-        var url = restaurantPath.menu_url;
-        // appending the restaurant name & their cuisine types
-        var newRow = $("<div>").append(
-          $("<div class='title'>").html('<a target="_blank" href="' + url + '">' + name + "</a>"),
-          $("<div class='cuisines'>").html("Cuisines: " + cuisines)
-        );
+      var name = restaurantPath.name;
+      var cuisines = restaurantPath.cuisines;
+      var url = restaurantPath.menu_url;
+      // appending the restaurant name & their cuisine types
+      var newRow = $("<div>").append(
+        $("<div class='title'>").html('<a target="_blank" href="' + url + '">' + name + "</a>"),
+        $("<div class='cuisines'>").html("Cuisines: " + cuisines)
+      );
       // appending the restaurant details to the div #tFood
       $("#tFood").append(newRow);
     };
@@ -120,10 +147,9 @@ function zomato(ctyId) {
 //EVENT LISTENER FOR BUTTON--------------------
 
 //click event to capture user input
-$(".btn-floating").on("click", function(){
+$(".btn-floating").on("click", function () {
 
-  if(flg)
-  {
+  if (flg) {
     //user entered bad query, reset page banner for next search
     $("#errorMsg").text("Spontaneity is a travel companion for spontaneous adventurers that want data-driven support for whimsical travel decision-making.  Enter a travel destination and a date range and Spontaneity will provide date-specific information on your potential destination.  This includes the weather forecast, local events, and excursions.");
     flg = false;
@@ -132,59 +158,52 @@ $(".btn-floating").on("click", function(){
   var $input = $("#icon_prefix").val().trim(); //get input
 
   //following two arrays for input validation
-  var nums = ["1","2","3","4","5","6","7","8","9","0"];
-  var spclChars = [ "!", "@", "#","$","%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "{", "]", "}", ":", ";", "<", ">", "?", '"', "'", ".", '/', "?", "|", "\\"];
+  var nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var spclChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "{", "]", "}", ":", ";", "<", ">", "?", '"', "'", ".", '/', "?", "|", "\\"];
 
-  for(var i = 0; i < $input.length; i++)
-  {
-        //ensure there are no numbers in input, if so, ask for new input
-        for(var j = 0; j < nums.length; j++) 
-        {
-          if($input[i] === nums[j])
-          {
-              $("#errorMsg").text("Please try again without using any numbers or special characters besides a comma.");
-              flg = true;
-              return;
-          }
-        }
-        //ensure there are no special characters in input, if so, ask for new input
-        for(var k = 0; k < spclChars.length; k++)
-        {
-          if($input[i] === spclChars[k])
-          {
-              $("#errorMsg").text("Please try again without using any numbers or special characters besides a comma.");
-              flg = true;
-              return;
-          }
-        }
-        //find the comma, and seperate the input into city and state vars
-        if($input[i] === ",")
-        {
-            var indx = i;
-            cty = $input.substr(0,indx);
-            stt = $input.substr(indx);
-            stt = stt.slice(1);
-            flg2 = true;
-        }
+  for (var i = 0; i < $input.length; i++) {
+    //ensure there are no numbers in input, if so, ask for new input
+    for (var j = 0; j < nums.length; j++) {
+      if ($input[i] === nums[j]) {
+        $("#errorMsg").text("Please try again without using any numbers or special characters besides a comma.");
+        flg = true;
+        return;
+      }
+    }
+    //ensure there are no special characters in input, if so, ask for new input
+    for (var k = 0; k < spclChars.length; k++) {
+      if ($input[i] === spclChars[k]) {
+        $("#errorMsg").text("Please try again without using any numbers or special characters besides a comma.");
+        flg = true;
+        return;
+      }
+    }
+    //find the comma, and seperate the input into city and state vars
+    if ($input[i] === ",") {
+      var indx = i;
+      cty = $input.substr(0, indx);
+      stt = $input.substr(indx);
+      stt = stt.slice(1);
+      flg2 = true;
+    }
   }
-        if(!flg2)
-        {
-            cty = $input;
-        }
+  if (!flg2) {
+    cty = $input;
+  }
 
-        database.ref().push({
+  database.ref().push({
 
-          city: cty,
-          dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
-  
+    city: cty,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
 
 
-       cityId();      
-       ticketmaster();
+  weather()
+  cityId();
+  ticketmaster();
 });
 
-database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
 
   $("#errorMsg").append(childSnapshot.val().city + " ");
 
